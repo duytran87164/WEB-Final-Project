add9ba99a76d178a0df4d184e4254eda0353004c
duytranus <duytranus5300@gmail.com>
2024-12-11 15:50:23 +0700

    Delete redundant files

--- offical/IOT_house/src/main/java/IOT_house/services/user/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package IOT_house.services.user;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "IOT_house") // Thay đổi nếu cần
-public class AppConfig {
-}
